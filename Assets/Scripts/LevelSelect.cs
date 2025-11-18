0c2fcf1c1b9d39481350021a9da211d30754125d
Simon Lekomtsev <semenlekomtsev@gmail.com>
2025-11-18 21:13:34 +0300

    repeat

---
+++ Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    public void LoadLevel(int levelIndex)
+    {
+        if (levelIndex >= 2 && levelIndex <= 9)
+        {
+            SceneManager.LoadScene(levelIndex);
+        }
+        else
+        {
+            Debug.LogWarning("Неверный номер уровня: " + levelIndex);
+        }
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
