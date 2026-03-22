f4c169205a2936fb68278a7a648b004618527a74
Toropov Danila <danatoropov9@gmail.com>
2026-03-22 10:45:38 +0300

    кнопка перехода из глав. меню в скин меню и обратно

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,6 +8,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
+    public void SkinMenu()
+    {
+        SceneManager.LoadScene(16);
+    }
+
     public void QuitGame()
     {
         Application.Quit();
